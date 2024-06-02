4879d0777a65c3b6432aa2cb8f19e84ad8afbbc9
DmitriiThe <>
2024-06-03 02:42:10 +0300

    changing page.module.css and completing page.tsx

--- skypro-music/src/app/page.tsx
+++ skypro-music/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-// import styles from "./page.module.css";
+// import Image from "next/image";
+import styles from "./page.module.css";
 import Player from "@/components/Player/Player";
 import Centerblock from "@/components/Centerblock/Centerblock";
 import Sidebar from "@/components/Sidebar/Sidebar";
@@ -7,15 +7,15 @@ import Nav from "@/components/Nav/Nav";
 
 export default function Home() {
   return (
-    <div className="wrapper">
-      <div className="container">
-        <main className="main">
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
           <Nav/>
           <Centerblock />
           <Sidebar />
         </main>
         <Player />
-        <footer className="footer" />
+        <footer className={styles.footer} />
       </div>
     </div>
   );
